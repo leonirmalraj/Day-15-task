49f1ee65c5c94f36827f77f6299ff4356df20437
LEO NIRMALRAJ <110334658+leonirmalraj@users.noreply.github.com>
2023-12-20 13:00:27 +0530

    Update script.js

--- Callback_Hell/script.js
+++ Callback_Hell/script.js
@@ -1,42 +1,55 @@
 // script.js
 
 const fetchData = (callback) => {
-    setTimeout(() => {
-        console.log('Data fetched');
-        const data = { message: 'Hello' };
-        callback(data);
-    }, 1000);
+  setTimeout(() => {
+    console.log('Data fetched');
+    const data = { message: 'Hello' };
+    callback(data);
+  }, 1000);
 };
 
 const processData = (data, callback) => {
-    setTimeout(() => {
-        console.log('Data processed:', data);
-        data.message += ' World';
-        callback(data);
-    }, 1000);
+  setTimeout(() => {
+    console.log('Data processed:', data);
+    data.message += ' World';
+    callback(data);
+  }, 1000);
 };
 
 const displayResult = (data) => {
-    setTimeout(() => {
-        console.log('Final result:', data.message);
-        document.getElementById('output-container').innerText = data.message;
-    }, 1000);
+  setTimeout(() => {
+    console.log('Final result:', data.message);
+    document.getElementById('output-container').innerText = data.message;
+  }, 1000);
 };
 
 const updateTimer = () => {
-    let seconds = 0;
-    return setInterval(() => {
-        seconds++;
-        document.getElementById('timer').innerText = seconds;
-    }, 1000);
+  let seconds = 0;
+  return setInterval(() => {
+    seconds++;
+    document.getElementById('timer').innerText = seconds;
+  }, 1000);
+};
+
+const countdown = (num, callback) => {
+  setTimeout(() => {
+    document.getElementById('countdown-container').innerText = num;
+    if (num > 1) {
+      countdown(num - 1, callback);
+    } else {
+      callback();
+    }
+  }, 1000);
 };
 
 // Initiating the callback hell
 const timerInterval = updateTimer();
 
-fetchData((data) => {
+countdown(10, () => {
+  fetchData((data) => {
     processData(data, (processedData) => {
-        displayResult(processedData);
-        clearInterval(timerInterval); // Stop the timer when the process is complete
+      displayResult(processedData);
+      clearInterval(timerInterval); // Stop the timer when the process is complete
     });
+  });
 });
